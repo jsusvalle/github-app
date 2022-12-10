657374de7e7276cc01d3579ad6d04cfb44eeb6bc
Jsusvalle <jesusvalle2001@hotmail.com>
2022-12-10 10:28:26 -0500

    feat: initial setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth/login',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
---
+++ styled.d.ts
@@ -0,0 +1,13 @@
+import "styled-components";
+
+declare module "styled-components" {
+    export interface DefaultTheme {
+        primary: string;
+        secondary: string;
+        white: string;
+        brightBlue: string;
+        silver: string;
+        darkGrey: string;
+        gray: string;
+    }
+}
